08961298f73739aba18a4b7c43c959f23d6471b4
Marcus Bjurman <mbjurman@gmail.com>
2013-08-23 21:00:08 +0200

    added second demo module

---
+++ fade-module.js
@@ -0,0 +1,46 @@
+define(
+    [
+        "jquery"
+    ],
+    function($) {
+
+        function FadeModule(el) {
+            var me = this;
+            me.$el = $(el);
+        
+            me.$content = me.$el.find("p");
+            me.$fadeIn = me.$el.find(".fade-in");
+            me.$fadeOut = me.$el.find(".fade-out");
+            me.opacity = parseInt(me.$content.css("opacity"));
+        }
+
+        FadeModule.prototype.init = function() {
+            var me = this;
+
+            me.$fadeIn.on("click", function() {
+                me.opacity = me.opacity + 0.1;
+                me.$content.css("opacity", me.opacity);
+            });
+
+            me.$fadeOut.on("click", function() {
+                me.opacity = me.opacity - 0.1;
+                me.$content.css("opacity", me.opacity);
+            });
+
+            return me;
+        };
+
+        FadeModule.prototype.dispose = function() {
+            this.$grow.off("click");
+            this.$shrink.off("click");
+        };
+
+        function init() {
+            return new FadeModule(this).init();
+        }
+
+        return {
+            init: init
+        };
+    }
+);
